2095f9eb3d4c1dfcc04598d6bf3345d1b29c92fd
Gabriel <gabriel.benini2@gmail.com>
2025-10-30 10:19:15 -0300

    Primeiro Commit

---
+++ src/test/java/com/gbenini/TaskFlow/TaskFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbenini.TaskFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
